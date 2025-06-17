5e8b8223799db1d8ab70d2e102e6ba7a27043db9
kaled182 <163221067+kaled182@users.noreply.github.com>
2025-06-17 22:17:36 +0100

    Update __init__.py

--- app/utils/__init__.py
+++ app/utils/__init__.py
@@ -1,10 +1,1 @@
-import os
-from flask import Flask
-from .routes import main_routes
 
-def create_app():
-    app = Flask(__name__)
-    app.secret_key = os.environ.get("SECRET_KEY", "troque-esta-chave")
-    app.config['SESSION_COOKIE_NAME'] = 'drivemaps_session'
-    app.register_blueprint(main_routes)
-    return app
