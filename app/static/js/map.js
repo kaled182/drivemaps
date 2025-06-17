9b868d433f1aaa132ff0783a3a9f04ced4542d57
kaled182 <163221067+kaled182@users.noreply.github.com>
2025-06-17 23:13:48 +0100

    Update map.js

--- app/static/js/map.js
+++ app/static/js/map.js
@@ -1,15 +1,11 @@
 function initMap() {
-    // Centraliza no primeiro endereço, ou em um ponto padrão se não houver
-    const center = (typeof locations !== "undefined" && locations.length > 0)
-        ? { lat: locations[0].lat, lng: locations[0].lng }
-        : { lat: 41.504073, lng: -8.761827 };
+    const center = { lat: 41.504073, lng: -8.761827 };
 
     const map = new google.maps.Map(document.getElementById("map"), {
         zoom: 12,
         center: center,
     });
 
-    // Adiciona um marcador para cada localização
     if (typeof locations !== "undefined") {
         locations.forEach(loc => {
             new google.maps.Marker({
