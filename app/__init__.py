658e60412ab62b37dae3e7411e8f7526c0fa5092
kaled182 <163221067+kaled182@users.noreply.github.com>
2025-06-18 00:23:14 +0100

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,15 @@
 import os
 from flask import Flask
-from .routes import main_routes
+from app.models import db
+from app.routes import main_routes
 
 def create_app():
     app = Flask(__name__)
     app.secret_key = os.environ.get("SECRET_KEY", "troque-esta-chave")
     app.config['SESSION_COOKIE_NAME'] = 'drivemaps_session'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///enderecos.db'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    db.init_app(app)
+
     app.register_blueprint(main_routes)
     return app
