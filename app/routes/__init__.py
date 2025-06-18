dd4ceff17ef40763640781457046754259af25d9
kaled182 <163221067+kaled182@users.noreply.github.com>
2025-06-18 22:14:26 +0100

    Update __init__.py

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -1,12 +1,14 @@
+# app/routes/__init__.py
+
 from flask import Blueprint
+
 from .importacao import importacao_bp as importacao_routes
 from .preview import preview_bp as preview_routes
 from .gerar import gerar_routes
 from .api import api_routes
 
-main_routes = Blueprint('main', __name__)
-
-main_routes.register_blueprint(importacao_routes)
-main_routes.register_blueprint(preview_routes)
-main_routes.register_blueprint(gerar_routes)
-main_routes.register_blueprint(api_routes)
+def register_routes(app):
+    app.register_blueprint(importacao_routes)
+    app.register_blueprint(preview_routes)
+    app.register_blueprint(gerar_routes)
+    app.register_blueprint(api_routes)
