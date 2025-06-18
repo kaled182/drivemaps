2b4a28dac4665fe7c5efedc8c08a01f0a12f4121
kaled182 <163221067+kaled182@users.noreply.github.com>
2025-06-18 19:23:23 +0100

    Create __init__.py

---
+++ app/routes/__init__.py
@@ -0,0 +1,16 @@
+# app/routes/__init__.py
+
+from flask import Blueprint
+from .importacao import import_routes
+from .preview import preview_routes
+from .gerar import gerar_routes
+from .api import api_routes
+
+# Cria um blueprint principal para rotas
+main_routes = Blueprint('main', __name__)
+
+# Registra os sub-blueprints de rotas específicas
+main_routes.register_blueprint(import_routes)
+main_routes.register_blueprint(preview_routes)
+main_routes.register_blueprint(gerar_routes)
+main_routes.register_blueprint(api_routes)
